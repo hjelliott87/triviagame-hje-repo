50e541853e9c8869081b7514e2f8a2283d245dc5
Henry Elliott <hjelliott1987@gmail.com>
2019-10-26 16:03:21 -0500

    Only the timer is working. I am about to give up on everything

--- assets/triviagame.js
+++ assets/triviagame.js
@@ -48,189 +48,210 @@ var numberOfQuestions = 12;
 
 var intervalId;
 var clockRunning = false;
-var time = 0;
-
-hideStuff();
+var time = 60;
 
 $(document).ready(function() {
 
-  $("#start").on("click", function() {
-    startOfGame = setTimeout(function() {
-    }, 60000);
-    for (var i = 0; i < questions.length; i++) {
-      for (var j = 0; j <= questions[0]; j++) {
-        $("#answer1-1").on("click", answers1[0]);
-        $("#answer1-2").on("click", answers1[1]);
-        $("#answer1-3").on("click", answers1[2]);
-        $("#answer1-4").on("click", answers1[3]);
-        if (answers1[2] = answers[0]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[1]; j++) {
-        $("#answer2-1").on("click", answers2[0]);
-        $("#answer2-2").on("click", answers2[1]);
-        $("#answer2-3").on("click", answers2[2]);
-        $("#answer2-4").on("click", answers2[3]);
-        if (answers2[0] = answers[1]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[2]; j++) {
-        $("#answer3-1").on("click", answers3[0]);
-        $("#answer3-2").on("click", answers3[1]);
-        $("#answer3-3").on("click", answers3[2]);
-        $("#answer3-4").on("click", answers3[3]);
-        if (answers3[3] = answers[2]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[3]; j++) {
-        $("#answer4-1").on("click", answers4[0]);
-        $("#answer4-2").on("click", answers4[1]);
-        $("#answer4-3").on("click", answers4[2]);
-        $("#answer4-4").on("click", answers4[3]);
-        if (answers4[1] = answers[3]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[4]; j++) {
-        $("#answer5-1").on("click", answers5[0]);
-        $("#answer5-2").on("click", answers5[1]);
-        $("#answer5-3").on("click", answers5[2]);
-        $("#answer5-4").on("click", answers5[3]);
-        if (answers5[0] = answers[4]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[5]; j++) {
-        $("#answer6-1").on("click", answers6[0]);
-        $("#answer6-2").on("click", answers6[1]);
-        $("#answer6-3").on("click", answers6[2]);
-        $("#answer6-4").on("click", answers6[3]);
-        if (answers6[3] = answers[5]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[6]; j++) {
-        $("#answer7-1").on("click", answers7[0]);
-        $("#answer7-2").on("click", answers7[1]);
-        $("#answer7-3").on("click", answers7[2]);
-        $("#answer7-4").on("click", answers7[3]);
-        if (answers7[1] = answers[6]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[7]; j++) {
-        $("#answer8-1").on("click", answers8[0]);
-        $("#answer8-2").on("click", answers8[1]);
-        $("#answer8-3").on("click", answers8[2]);
-        $("#answer8-4").on("click", answers8[3]);
-        if (answers8[3] = answers[7]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[8]; j++) {
-        $("#answer9-1").on("click", answers9[0]);
-        $("#answer9-2").on("click", answers9[1]);
-        $("#answer9-3").on("click", answers9[2]);
-        $("#answer9-4").on("click", answers9[3]);
-        if (answers9[2] = answers[8]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[9]; j++) {
-        $("#answer10-1").on("click", answers10[0]);
-        $("#answer10-2").on("click", answers10[1]);
-        $("#answer10-3").on("click", answers10[2]);
-        $("#answer10-4").on("click", answers10[3]);
-        if (answers10[0] = answers[9]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[10]; j++) {
-        $("#answer11-1").on("click", answers11[0]);
-        $("#answer11-2").on("click", answers11[1]);
-        $("#answer11-3").on("click", answers11[2]);
-        $("#answer11-4").on("click", answers11[3]);
-        if (answers11[1] = answers[10]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
-      for (var j = 0; j <= questions[11]; j++) {
-        $("#answer12-1").on("click", answers12[0]);
-        $("#answer12-2").on("click", answers12[1]);
-        $("#answer12-3").on("click", answers12[2]);
-        $("#answer12-4").on("click", answers12[3]);
-        if (answers12[3] = answers[11]) {
-          correctAnswer = true;
-          coreect++;
-         } else {
-           correctAnswer = false;
-           wrong++;
-         }
-      }
+  function answerList() {
+    
+  }
+  
+  for (var i = 0; i < questions.length; i++) {
+    for (var j = 0; j <= questions[0]; j++) {
+      $("#answer1-1").on("click", answerList);
+      $("#answer1-2").on("click", answerList);
+      $("#answer1-3").on("click", answerList);
+      $("#answer1-4").on("click", answerList);
+      if (answers1[2] == answers[0]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
     }
+    for (var j = 0; j <= questions[1]; j++) {
+      $("#answer2-1").on("click", answers2[0]);
+      $("#answer2-2").on("click", answers2[1]);
+      $("#answer2-3").on("click", answers2[2]);
+      $("#answer2-4").on("click", answers2[3]);
+      if (answers2[0] == answers[1]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[2]; j++) {
+      $("#answer3-1").on("click", answers3[0]);
+      $("#answer3-2").on("click", answers3[1]);
+      $("#answer3-3").on("click", answers3[2]);
+      $("#answer3-4").on("click", answers3[3]);
+      if (answers3[3] = answers[2]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[3]; j++) {
+      $("#answer4-1").on("click", answers4[0]);
+      $("#answer4-2").on("click", answers4[1]);
+      $("#answer4-3").on("click", answers4[2]);
+      $("#answer4-4").on("click", answers4[3]);
+      if (answers4[1] = answers[3]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[4]; j++) {
+      $("#answer5-1").on("click", answers5[0]);
+      $("#answer5-2").on("click", answers5[1]);
+      $("#answer5-3").on("click", answers5[2]);
+      $("#answer5-4").on("click", answers5[3]);
+      if (answers5[0] = answers[4]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[5]; j++) {
+      $("#answer6-1").on("click", answers6[0]);
+      $("#answer6-2").on("click", answers6[1]);
+      $("#answer6-3").on("click", answers6[2]);
+      $("#answer6-4").on("click", answers6[3]);
+      if (answers6[3] = answers[5]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[6]; j++) {
+      $("#answer7-1").on("click", answers7[0]);
+      $("#answer7-2").on("click", answers7[1]);
+      $("#answer7-3").on("click", answers7[2]);
+      $("#answer7-4").on("click", answers7[3]);
+      if (answers7[1] = answers[6]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[7]; j++) {
+      $("#answer8-1").on("click", answers8[0]);
+      $("#answer8-2").on("click", answers8[1]);
+      $("#answer8-3").on("click", answers8[2]);
+      $("#answer8-4").on("click", answers8[3]);
+      if (answers8[3] = answers[7]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[8]; j++) {
+      $("#answer9-1").on("click", answers9[0]);
+      $("#answer9-2").on("click", answers9[1]);
+      $("#answer9-3").on("click", answers9[2]);
+      $("#answer9-4").on("click", answers9[3]);
+      if (answers9[2] = answers[8]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[9]; j++) {
+      $("#answer10-1").on("click", answers10[0]);
+      $("#answer10-2").on("click", answers10[1]);
+      $("#answer10-3").on("click", answers10[2]);
+      $("#answer10-4").on("click", answers10[3]);
+      if (answers10[0] = answers[9]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[10]; j++) {
+      $("#answer11-1").on("click", answers11[0]);
+      $("#answer11-2").on("click", answers11[1]);
+      $("#answer11-3").on("click", answers11[2]);
+      $("#answer11-4").on("click", answers11[3]);
+      if (answers11[1] = answers[10]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+    for (var j = 0; j <= questions[11]; j++) {
+      $("#answer12-1").on("click", answers12[0]);
+      $("#answer12-2").on("click", answers12[1]);
+      $("#answer12-3").on("click", answers12[2]);
+      $("#answer12-4").on("click", answers12[3]);
+      if (answers12[3] = answers[11]) {
+        correctAnswer = true;
+        coreect++;
+       } else {
+         correctAnswer = false;
+         wrong++;
+       }
+    }
+  }
+
+  $("#start").on("click", function() {
+    startOfGame = setInterval(count, 1000);
+    clockRunning = true;
+    hideStuff;
+    // console.log(time);
   });
 
 });
 
 $("#finish").on("click", function() {
+  clearInterval(startOfGame); 
   $("#correct").text(correct);
   $("#wrong").text(wrong);
   clearTimeout(endOfGame);
+  console.log(correct);
+  console.log(wrong);
 });
+
+function hideStuff() {
+  var hide = document.getElementById("#quiz");
+  if (hide.style.display === "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+}
+
 function count() {
   time--;
   var converted = timeConverter(time);
   console.log(converted);
-
+  if (time == 0){
+    clearInterval(startOfGame);
+  }
   $("#display").text(converted);
 }
+
 function timeConverter(t) {
 
   var minutes = Math.floor(t / 60);
